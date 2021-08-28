108b9c11e5809d1ea61ad445229f8bd5c02b0901
Claudia <claudiaeduarda_duda@hotmail.com>
2021-08-28 20:59:38 -0300

    basic structure

---
+++ src/main/java/com/simplestore/myfirststore/repositories/CategoryRepository.java
@@ -0,0 +1,28 @@
+package com.simplestore.myfirststore.repositories;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+import com.simplestore.myfirststore.entities.Category;
+
+@Component
+public class CategoryRepository {
+	
+	private Map<Long, Category> map = new HashMap<>();
+	
+	public void save(Category category) {
+		map.put(category.getId(), category);
+	}
+
+	public Category findById(Long id) {
+		return map.get(id);
+	}
+	
+	public List<Category> findAll() {
+		return new ArrayList<Category>(map.values());
+	}
+}
---
+++ src/test/java/com/simplestore/myfirststore/MyfirststoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplestore.myfirststore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirststoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
